ea3651ae8c6a9453b0beec37890975b98a787365
ac.mateus <ac.mateus@campus.fct.unl.pt>
2025-04-10 14:14:57 +0100

    Fix requirements.txt for Railway build

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def serve_live_image():
         return "Waiting for live feed...", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8080)
